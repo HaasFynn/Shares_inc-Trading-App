5ff3ac3bb097f55f528819376af5391b080273a9
fhaas <fynngian@gmail.com>
2024-05-21 13:38:47 +0200

    restored stashed changes

--- src/main/java/javafx/SharesInc.java
+++ src/main/java/javafx/SharesInc.java
@@ -1,32 +1,21 @@
-package javafx.share_creation;
+package javafx;
 
-import javafx.ScreenBuilder;
+import javafx.assets.ScreenBuilder;
 import javafx.application.Application;
+import javafx.login.LoginPane;
 import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Font;
+import javafx.share_creation.ShareCreatorPane;
 import javafx.stage.Stage;
 
-public class temp_ShareCreatorMain extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+public class SharesInc extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
         Region sceneRoot = new ScreenBuilder().build();
         Scene scene = new Scene(sceneRoot);
-        scene.setRoot(new ShareCreatorPane(Font.font("Verdana")));
-        editStage(stage, scene);
-        stage.show();
-    }
-
-    public void start(Font font) {
-        Stage stage = new Stage();
-        Region sceneRoot = new ScreenBuilder().build();
-        Scene scene = new Scene(sceneRoot);
-        scene.setRoot(new ShareCreatorPane(font));
+        scene.setRoot(new ShareCreatorPane("Verdana", scene) /*new LoginPane("Verdana", scene)*/);
         editStage(stage, scene);
         stage.show();
     }
